4ba77ce4a195dd197cf35c28f0fdb8a504ef46de
Gustavo <pgustavo_raul@yahoo.com>
2018-08-13 22:38:23 -0500

    paginas hbs

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require('express')
 const app = express()
+const hbs = require('hbs');
+
+require('./hbs/helpers')
 
 app.use(express.static(__dirname + '/public'));
 
-// app.get('/', function(req, res) {
-//     let salida = {
-//         nombre: 'gustavo',
-//         edad: 37,
-//         url: req.url
-//     }
-//     res.send(salida);
-// })
+//express hbs engine
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.get('/', function(req, res) {
+    res.render('home', {
+        nombre: 'gustavo'
+    });
+})
+
+app.get('/about', function(req, res) {
+    res.render('about');
+})
 
 app.listen(3000, () => {
     console.log('Escuchando peticiones en el puerto 3000');
