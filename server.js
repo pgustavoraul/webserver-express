337d6adbae2549b219f023851bacc252ae8c9a8d
Gustavo <pgustavo_raul@yahoo.com>
2018-08-14 21:54:53 -0500

    config heroku

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const hbs = require('hbs');
 
 require('./hbs/helpers')
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + '/public'));
 
 //express hbs engine
@@ -21,6 +23,6 @@ app.get('/about', function(req, res) {
     res.render('about');
 })
 
-app.listen(3000, () => {
-    console.log('Escuchando peticiones en el puerto 3000');
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port }`);
 })
